af7109f0352e2f6b450437aed32ed289322786a1
PIESBOY-BRO <87906656+PIESBOYBRO@users.noreply.github.com>
2021-11-24 17:48:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,13 +12,13 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Movie world is a open source project. 
-- Source - https://github.com/PIESBOYBRO/EvaMaria.git  
+- Source - https://github.com/PIESBOYBRO/MOVIE-WORLD  
 
 <b>DEVS:</b>
 - <a href=https://t.me/Movie_World_00> 𝗠𝗼𝘃𝗶𝗲 𝘄𝗼𝗿𝗹𝗱📽™ </a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Movieworld will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. eva maria should have admin privillage.
@@ -32,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Movie World Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. Movie World supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/movie_world_00_bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Movie World
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
