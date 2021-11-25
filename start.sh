28ed93acdaf940a15888c297a6943106b8292ed6
PIESBOY-BRO <87906656+PIESBOYBRO@users.noreply.github.com>
2021-11-25 14:03:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/PIESBOYBRO/MOVIE-WORLD /PIESBOYBRO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /PIESBOYBRO
 fi
-cd /EvaMaria
+cd /PIESBOYBRO
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
