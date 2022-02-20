ccd7e70f7b60dca21d3f361e462eac2861d8c593
PIESBOY-BRO <87906656+PIESBOYBRO@users.noreply.github.com>
2022-02-20 23:17:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -436,7 +436,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Movie_World_00')
+            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/MovieWorld_Support')
+ 𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣 ⎆')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -466,7 +467,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Movie_World_00'),
+            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/MovieWorld_Support'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
             ],[
             InlineKeyboardButton('🏠 Home', callback_data='start'),
